0bb3709991b29b595c275b0f48beaf955b3d8d69
bidursapkota00 <bidursapkota00@gmail.com>
2025-05-01 00:10:49 +0545

    everything

---
+++ youtube-webclient/src/app/firebase/firebasee.ts
@@ -0,0 +1,11 @@
+// import { initializeApp } from "firebase/app";
+
+// const firebaseConfig = {
+//   apiKey: "",
+//   authDomain: "",
+//   projectId: "",
+//   appId: "",
+// };
+
+// // Initialize Firebase
+// export const app = initializeApp(firebaseConfig);
